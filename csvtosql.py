573483b9f72dd2d6e5482705719f3a16e54b5a38
spetykowski <spetykowski@gmail.com>
2017-02-10 15:12:22 -0500

    Support for Different String Representation
    Closes #2
    
    CSVtoSQL will now support for string values with a trailing single quote or when surrounded by single quotes. Updated variable names to match python style guide.

--- csvtosql.py
+++ csvtosql.py
@@ -1,4 +1,4 @@
-# v0.3.2
+# v0.3.3
 
 # Import Resources
 import csv
@@ -14,87 +14,86 @@ parser.add_argument('-t', '--table', nargs=1, help='Use this flag to define the 
 parser.add_argument('-l', '--last', nargs=1, help='Limit generation to number of rows defined, starting from end of csv.', default=[0])
 args = parser.parse_args()
 
-# Set Table Name
-tablename = args.table[0]
+# Set User-Defined Arg Variables
+table_name = args.table[0]
+data_to_update = args.file[0]
+limit_generation_by = int(args.last[0])
 
-# Determine Platform OS
-platform = platform.system()
+# Determine Platform Operating System
+operating_system = platform.system()
 
-# Get User Desktop
-if platform == 'Darwin':
-	Filename = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop/output-' + tablename + '.sql')
-elif platform == 'Windows':
-	Filename = os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + '\\Desktop\output-' + tablename + '.sql'
-	
-# Set File Paths
-if platform == 'Darwin':
-	sqlplaceholderfilepath = 'INSERT/statement.txt'
-elif platform == 'Windows':
-	sqlplaceholderfilepath = 'INSERT\statement.txt'
-	
-datatoupdate = args.file[0]
-limitgenerationby = int(args.last[0])
+# Configure OS Specific Variables
+if operating_system == 'Darwin':
+	file_name = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop/output-' + table_name + '.sql')
+	sql_placeholder_file_path = 'INSERT/statement.txt'
+elif operating_system == 'Windows':
+	file_name = os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + '\\Desktop\output-' + table_name + '.sql'
+	sql_placeholder_file_path = 'INSERT\statement.txt'
 
 if args.command == "INSERT":
 
 	# Create File
-	with open(Filename, "wt") as outfile:
+	with open(file_name, "wt") as outfile:
 		outfile.close
 	
 	# Get SQL INSERT Script Placeholder
-	sqlplaceholder = open(sqlplaceholderfilepath, "rt")
-	sqldata = sqlplaceholder.read()
-	sqlplaceholder.close()
+	sql_placeholder = open(sql_placeholder_file_path, "rt")
+	sql_placeholder_data = sql_placeholder.read()
+	sql_placeholder.close()
 	
 	# Open CSV and build dictionary
-	with open(datatoupdate, newline='') as csvfile:
-		dictcsv = csv.DictReader(csvfile)
-		fields = dictcsv.fieldnames
-		rowsaslist = []
-
-		fieldnames = ''
+	with open(data_to_update, newline='') as csvfile:
+		csv_dictionary = csv.DictReader(csvfile)
+		cvs_field_keys = csv_dictionary.fieldnames
+		row_values_list = []
+		csv_field_names = ''
 		
-		for field in fields:
-			fieldnames += field
-			if field != fields[-1]:
-				fieldnames += ', '
+		# Create String of Field Names
+		for field_name in cvs_field_keys:
+			csv_field_names += field_name
+			# Skip comma if last item in array
+			if field_name != cvs_field_keys[-1]:
+				csv_field_names += ', '
 		
-		for row in dictcsv:
+		for row in csv_dictionary:
 			x = 0
 			items = []
-			columnvalue = ''
-			replaceColumnNames = ''
-			replaceColumnNames = sqldata.replace("<COLUMN_NAMES>", fieldnames)
+			column_value = ''
+			replace_column_names = ''
+			replace_column_names = sql_placeholder_data.replace("<COLUMN_NAMES>", csv_field_names)
 			
-			for field in fields:
-				checkitem = row[fields[x]]
-				if checkitem == '':
-					checkitem = 'null'
-				if checkitem.endswith("'"):
-					checkitem = "'" + checkitem
-				items.append(checkitem)
-				if field != fields[-1]:
+			for field_name in cvs_field_keys:
+				field_value = row[cvs_field_keys[x]]
+				if field_value == '':
+					field_value = 'null'
+				if field_value.endswith("'") and not field_value.startswith("'") :
+					field_value = "'" + field_value
+				items.append(field_value)
+				# Skip comma if last item in array
+				if field_name != cvs_field_keys[-1]:
 					items.append(', ')
 				x = x + 1
 			
-			columnvalue = ''.join(items)
-			rowsaslist.append(columnvalue)
+			column_value = ''.join(items)
+			row_values_list.append(column_value)
+
+		# Generate 
+		for row_item in row_values_list[-limit_generation_by:]:
+			column_values = ''
+			merged_column_values = ''.join(row_item)
+			column_values += merged_column_values
+			replace_values_in_statement = replace_column_names.replace("<COLUMN_VALUES>", column_values)
+			replace_table_in_statement = replace_values_in_statement.replace("<TABLE_NAME>", table_name)
 			
-		for rowitem in rowsaslist[-limitgenerationby:]:
-			columnvalues = ''
-			testing = ''.join(rowitem)
-			columnvalues += testing
-			replaceItems = replaceColumnNames.replace("<COLUMN_VALUES>", columnvalues)
-			replaceTable = replaceItems.replace("<TABLE_NAME>", tablename)
-			
-# 		Write INSERT wrap up
-			with open(Filename, "at") as outfile:
-				outfile.write(replaceTable + '\n')
+			# Write INSERT wrap up
+			with open(file_name, "at") as outfile:
+				outfile.write(replace_table_in_statement + '\n')
 				outfile.close
 				
-# 	Write INSERT Commit
-	with open(Filename, "at") as outfile:
+	# Write INSERT Commit
+	with open(file_name, "at") as outfile:
 		outfile.write('COMMIT;')
 		outfile.close
 	
-	print('\nCSVtoSQL Completed Sucessfully - Check your desktop for the completed script at output-' + tablename + '.sql')
+	# Print Success To User
+	print('\nCSVtoSQL Completed Sucessfully - Check your desktop for the completed script at output-' + table_name + '.sql')
